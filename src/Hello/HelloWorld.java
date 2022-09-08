37ca6815cdf47409f3fc2d1516b06684c18a2b42
AINNG <ainngng@gmail.com>
2022-09-08 23:49:51 +0800

    Initial commit

---
+++ src/Hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package Hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+    }
+    String [] args = new String[]{"aa","afdsaf","sadfa"};
+    
+   
+}
+
+//第一次使用idea书写java代码 2022-9-7
---
+++ src/Hello/test.java
@@ -0,0 +1,8 @@
+package Hello;
+
+/**
+ * @author 王子安
+ * @date 2022/9/7 - 22:18
+ */
+public class test {
+}
